23818e8cb1c8d0ac54388535a90e7d67985b3ef2
Pedro Chaves <pedroochavess97@gmail.com>
2020-08-25 19:57:46 -0300

    api of transactions using nodejs

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,40 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('This type of transaction is invalid');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw Error(
+        'This transaction is not possible now! Try to get more income transactions.',
+      );
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
